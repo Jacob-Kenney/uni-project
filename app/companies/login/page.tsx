96ab5dae7199e4233636462beae4c65da6cb53d7
Jacob Kenney <jacob.t.kenney1@gmail.com>
2025-06-24 16:04:43 +0100

    fix: fixed header state being initialised as logged in before session could load

---
+++ app/companies/login/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { SignIn } from "@/components/companies-sign-in";
+
+export default function Page() {
+    return (
+        <SignIn />
+    )
+}
--- components/header.tsx
+++ components/header.tsx
@@ -32,7 +32,10 @@ export default function Header() {
                     */}
 
                     {/* Login / account management buttons */}
-                    { session? (
+                    { status === "loading" ? (
+                        // Loading state
+                        <div className="h-10 w-20 bg-gray-200 animate-pulse rounded-md"></div>
+                    ) : session? (
                         <div className="hidden md:flex items-center gap-4">
                             <Link href="/users/login">
                                 <button className="text-lg text-brand-primary hover:bg-brand-primary/30 transition-all duration-300 hover:scale-105 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 h-10 px-4 py-2">
@@ -70,7 +73,10 @@ export default function Header() {
                 {mobileMenuOpen && (
                     <div className="md:hidden backdrop-blur-sm border-t border-gray-200/50">
                         {/* Login / account management buttons */}
-                        {session? (
+                        { status === "loading" ? (
+                            // Loading state
+                            <div className="h-10 w-full bg-gray-200 animate-pulse rounded-md"></div>
+                        ) : session? (
                             <div className="container mx-auto flex flex-col gap-3">
                                 <Link href="/users/login">
                                     <button className="justify-start text-lg text-brand-primary hover:bg-brand-primary/30 transition-all duration-300 hover:scale-105 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 h-10 px-4 py-2">
