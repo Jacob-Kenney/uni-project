e646d76b8369501d8369321eb226fd7bec8f8e74
Jacob Kenney <jacob.t.kenney1@gmail.com>
2025-06-24 15:56:41 +0100

    feat: updated header to allow sign out when logged in

--- components/header.tsx
+++ components/header.tsx
@@ -2,11 +2,13 @@
 import { useState } from "react"
 import Image from "next/image"
 import Link from "next/link"
-import { X, Menu } from "lucide-react";
+import { X, Menu, LogOut, User } from "lucide-react";
+import { useSession, signOut } from "next-auth/react";
 
 export default function Header() {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-    
+    const { data: session, status } = useSession()
+
     return (
         <header className="fixed w-full backdrop-blur-sm">
             <div className="px-12 lg:px-24 xl:px-32 py-6">
@@ -29,19 +31,31 @@ export default function Header() {
                     </div>
                     */}
 
-                    {/* Login buttons */}
-                    <div className="hidden md:flex items-center gap-4">
-                        <Link href="/users/login">
-                            <button className="text-lg text-brand-primary hover:bg-brand-primary/30 transition-all duration-300 hover:scale-105 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 h-10 px-4 py-2">
-                                Sign in
+                    {/* Login / account management buttons */}
+                    { session? (
+                        <div className="hidden md:flex items-center gap-4">
+                            <Link href="/users/login">
+                                <button className="text-lg text-brand-primary hover:bg-brand-primary/30 transition-all duration-300 hover:scale-105 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 h-10 px-4 py-2">
+                                    Sign in
+                                </button>
+                            </Link>
+                            <Link href="companies/login">
+                                <button className="text-lg bg-brand-primary hover:bg-brand-primary/70 text-white transition-all duration-300 hover:scale-105 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 h-10 px-4 py-2">
+                                    Post a job
+                                </button>
+                            </Link>
+                        </div>
+                    ) : (
+                        <div className="hidden md:flex items-center gap-4">
+                            <button onClick={() => signOut()} className="text-lg text-brand-primary hover:bg-brand-primary/30 transition-all duration-300 hover:scale-105 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 h-10 px-4 py-2">
+                                Sign out
                             </button>
-                        </Link>
-                        <Link href="companies/login">
-                            <button className="text-lg bg-brand-primary hover:bg-brand-primary/70 text-white transition-all duration-300 hover:scale-105 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 h-10 px-4 py-2">
-                                Post a job
-                            </button>
-                        </Link>
-                    </div>
+                            {/* Link to user account management page (business or user) */}
+                            <Link href="/">
+                                <User size={16} />
+                            </Link>
+                        </div>
+                    )}
 
                     {/* Mobile menu */}
                     <button 
@@ -55,18 +69,31 @@ export default function Header() {
                 {/* Mobile menu dropdown */}
                 {mobileMenuOpen && (
                     <div className="md:hidden backdrop-blur-sm border-t border-gray-200/50">
-                        <div className="container mx-auto flex flex-col gap-3">
-                            <Link href="/users/login">
-                                <button className="justify-start text-lg text-brand-primary hover:bg-brand-primary/30 transition-all duration-300 hover:scale-105 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 h-10 px-4 py-2">
-                                    Sign in
+                        {/* Login / account management buttons */}
+                        {session? (
+                            <div className="container mx-auto flex flex-col gap-3">
+                                <Link href="/users/login">
+                                    <button className="justify-start text-lg text-brand-primary hover:bg-brand-primary/30 transition-all duration-300 hover:scale-105 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 h-10 px-4 py-2">
+                                        Sign in
+                                    </button>
+                                </Link>
+                                <Link href="companies/login">
+                                    <button className="justify-start text-lg bg-brand-primary hover:bg-brand-primary/70 text-white transition-all duration-300 hover:scale-105 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 h-10 px-4 py-2">
+                                        Post a job
+                                    </button>
+                                </Link>
+                            </div>
+                        ) : (
+                            <div className="container mx-auto flex flex-col gap-3">
+                                <button onClick={() => signOut()} className="text-lg text-brand-primary hover:bg-brand-primary/30 transition-all duration-300 hover:scale-105 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 h-10 px-4 py-2">
+                                    Sign out
                                 </button>
-                            </Link>
-                            <Link href="companies/login">
-                                <button className="justify-start text-lg bg-brand-primary hover:bg-brand-primary/70 text-white transition-all duration-300 hover:scale-105 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 h-10 px-4 py-2">
-                                    Post a job
-                                </button>
-                            </Link>
-                        </div>
+                                {/* Link to user account management page (business or user) */}
+                                <Link href="/">
+                                    <User size={16} />
+                                </Link>
+                            </div>
+                        )}
                     </div>                
                 )}
             </div>
